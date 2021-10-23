c65c31681853e992d757ccf09dbca81ceb92c9d2
j5pux <63794670+j5pux@users.noreply.github.com>
2021-10-23 12:31:56 +0200

    auto

---
+++ bin/pre_commit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Pre commit hook script
+
+#######################################
+# Create info files with btrap args and compat scripts
+main() {
+  echo pre-commit
+}
+
+main
